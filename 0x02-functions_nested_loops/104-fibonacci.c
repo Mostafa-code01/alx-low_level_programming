62f9ca81f701756011b863829ef977fc56b51dcf
Mostafa-code01 <mostafadark201018@gmail.com>
2023-10-10 12:06:37 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
  *
  * Purpose - no hardcode
  *
- * Return:  (Success)
+ * Return: (Success)
  */
 
 int main(void)
@@ -21,44 +21,28 @@ int main(void)
 
 	printf("%lu", bef);
 
-	for (i = 1; i < 91; i++)
+	for (i = 1; i <= 91; i++)
 	{
 		printf(", %lu", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
 
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
+	bef1 = bef / l;
+	bef2 = bef % l;
+	aft1 = aft / l;
+	aft2 = aft % l;
 
-	for (i = 92; i < 99; ++i)
+	for (i = 92; i <= 99; ++i)
 	{
 		printf(", %lu", aft1 + (aft2 / l));
 		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
 		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
 		bef2 = aft2 - bef2;
+		aft1 = aft1 + bef1;
+		aft2 = aft2 + bef2;
 	}
+
 	printf("\n");
 	return (0);
-}
-
-
-_putchar.c
-
-#include "main.h"
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
