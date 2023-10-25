eb264aa6d21f8f846c5ae852e020e4cd958ebb05
Mostafa-code01 <mostafadark201018@gmail.com>
2023-10-25 12:46:35 -0700

    :4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,15 +5,12 @@
   * @y: parameter 2
   * Return: value of a given number passed to the function
   */
-
-
 int _pow_recursion(int x, int y)
 {
 
-if (y < 0)
-        return (-1);
-if (y == 0)
-        return (1);
+        if (y < 0)
+                return (-1);
+        if (y == 0)
+                return (1);
         return (x * _pow_recursion(x, y - 1));
-
 }
